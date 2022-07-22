da2c96f848c92bda7ff15cff533ec09f8b9d7cc9
Isaiah <ijandicos90@gmail.com>
2022-07-22 17:45:12 -0400

    commit tree de5cdd08ad59cfd86d2c2344d223046a63aefd90

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
+require "pry"
+
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+ 
+
